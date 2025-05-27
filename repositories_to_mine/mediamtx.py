a4051b995d369fa1765f3d150a3d1fb6e56a36ae
rafabpg <rafa23giordano@gmail.com>
2025-05-27 11:29:18 -0300

    fix: fixing mediamtx analises

--- repositories_to_mine/mediamtx.py
+++ repositories_to_mine/mediamtx.py
@@ -9,13 +9,3 @@ class MediamtxIssues(BaseGithubIssues):
     
     def get_bug_labels(self):
         return '["bug"]'
-    
-    def get_excluded_labels(self):
-        return ["status: needs triage", "duplicate", "invalid"]
-    
-    def get_special_filters(self):
-        return [
-            lambda issue: any(
-                keyword in issue["title"].lower() for keyword in ["error", "fail", "panic", "crash", "unexpected"]
-            )
-        ]
